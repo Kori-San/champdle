e6a116cf1f253fc1e44968ceb1ebc1185a2f33b2
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2023-04-03 00:54:33 +0200

    [REFACTOR] Language linked changes

--- src/spell/scripts/index.js
+++ src/spell/scripts/index.js
@@ -1,8 +1,4 @@
-import { initLanguage } from "/lib/language.js";
 import { game } from "./game.js";
-
-/* Initializing the language of the game. */
-initLanguage();
 
 /* Launching the game */
 game();
